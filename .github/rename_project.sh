83915daac7eff85706bb004ddc9077c6a92d4c0c
Bruno Rocha <rochacbruno@users.noreply.github.com>
2021-08-29 01:07:32 +0100

    Update rename_project.sh

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -33,6 +33,6 @@ done
 mv project_name $name
 
 # This command runs only once on GHA!
-if [ -f .github/workflows/project_name.yml ]; then
+if [ -f .github/workflows/rename_project.yml ]; then
     mv .github/workflows/rename_project.yml .github/workflows/rename_project.yml.disabled
 fi
